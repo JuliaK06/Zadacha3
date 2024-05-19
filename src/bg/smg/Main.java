0a069e0356d01a6f0cc29a5834433978b7de8ea5
JuliaK06 <kozarova.julia@gmail.com>
2024-05-19 14:47:46 +0300

    Small correction

--- src/bg/smg/Main.java
+++ src/bg/smg/Main.java
@@ -8,8 +8,7 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.concurrent.PriorityBlockingQueue;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
         PriorityBlockingQueue<Clothes> list = new PriorityBlockingQueue<>();
